deb76c3c35571be824024aa8f9b9d3bed214fa2d
Maryadi <maryadi@elephanttalk.com>
2016-05-23 14:47:29 +0700

    Finishing Alpha animation....

--- app/src/main/java/hisoka/poipo/com/animation/MainActivity.java
+++ app/src/main/java/hisoka/poipo/com/animation/MainActivity.java
@@ -1,7 +1,12 @@
 package hisoka.poipo.com.animation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView satu = (TextView) findViewById(R.id.tv1);
+        Button button1 = (Button) findViewById(R.id.bt1);
+        final Animation a = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        a.reset();
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                satu.startAnimation(a);
+            }
+        });
+
     }
 }
